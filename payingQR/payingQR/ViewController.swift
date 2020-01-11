8f8a88fd15630eb503670abb0065c5963e05bf5c
alejandro <scanf16@hotmail.com>
2020-01-11 04:19:07 -0500

    First Commit

---
+++ payingQR/payingQR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  payingQR
+//
+//  Created by alejandro david perez morales on 1/11/20.
+//  Copyright © 2020 alejandro david perez morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
